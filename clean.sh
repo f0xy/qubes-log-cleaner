bcaf8085b303b0b242de334ab4049c504d6b6be0
f0xy <46729782+f0xy@users.noreply.github.com>
2020-09-10 11:17:47 -0700

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -5,3 +5,5 @@ sudo journalctl --vacuum-time=1s
 #journalctl --rotate
 #journalctl --vacuum-time=1s
 rm -frv /var/log/qubes/*
+sudo rm -frv /var/log/xen/console/*
+sudo rm -frv /var/log/libvirt/libxl/
