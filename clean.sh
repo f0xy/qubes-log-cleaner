d911aad65770cb20559b8e12066f6f6058be1511
f0xy <46729782+f0xy@users.noreply.github.com>
2021-12-20 13:44:15 -0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -7,4 +7,5 @@ sudo journalctl --vacuum-time=1s
 rm -frv /var/log/qubes/*
 sudo rm -frv /var/log/xen/console/*
 sudo rm -frv /var/log/libvirt/libxl/
+sudo rm -frv /var/lib/logrotate/logrotate.status
 #Reboot system after running script mostly due to removing the libxl folder
